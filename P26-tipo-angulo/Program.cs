8824d4e2dbc0a1941c64e2bff88b32968267e90e
38192392@uaz.edu.mx <38192392@uaz.edu.mx>
2023-02-23 15:53:58 -0600

    Actividad 5

---
+++ P26-tipo-angulo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+
+Console.WriteLine("Muestra el tipo de angulo según los grados :\n");
+Console.Write("Dame un angulo entre 0 y 360 grados : ");
+int angulo = int.Parse(Console.ReadLine());
+
+if(angulo < 0 && angulo >360){
+    Console.WriteLine("Angulo invalido ...");
+} 
+else
+ {
+    if(angulo<90) Console.WriteLine("\n El angulo es AGUDO..");
+    if(angulo==90) Console.WriteLine("\n El angulo es RECTO..");
+    if(angulo>90 && angulo<180) Console.WriteLine("\n El angulo es OBTUSO..");
+    if(angulo==180) Console.WriteLine("\n El angulo es LLANO..");
+    if(angulo>180 && angulo<360) Console.WriteLine("\n El angulo es CONCAVO..");
+    if(angulo==360) Console.WriteLine("\n El angulo es COMPLETO..");
+}
+Console.WriteLine("\n Preoceso termindo");
---
+++ P27-aceptar-estudiante/Program.cs
@@ -0,0 +1,19 @@
+﻿// Acepta a un estudiante en base a su edad y sus calificaciones
+Console.Clear();
+Console.WriteLine("\n Acepta a un estudiante en base a su edad y sus calificaciones \n");
+Console.Write("Dame tu nombre ?");
+string nombre = Console.ReadLine();
+Console.Write("Dame tu edad ?");
+int edad = int.Parse(Console.ReadLine());
+
+if( edad < 18 ) Console.WriteLine("\n NO aceptamos menores de edad, BYE");
+else{
+    Console.Write("Calificacion 1 ?"); float c1 = float.Parse(Console.ReadLine());
+    Console.Write("Calificacion 2 ?"); float c2 = float.Parse(Console.ReadLine());
+    if( c1<8 || c2 >8) 
+    Console.WriteLine("\n No aceptamos con calificación menor a 8");
+    else{
+        Console.WriteLine($"BIENVENIDO {nombre} tienes {edad} y calificaciones {c1} y {c2}");
+      }
+    }
+Console.WriteLine( "\n Gracias por participar");
