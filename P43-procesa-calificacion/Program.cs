f062ef8662a454721d68d6ad0cf010d4e0ec42e4
38192392@uaz.edu.mx <38192392@uaz.edu.mx>
2023-03-09 22:04:49 -0600

    Actividad 8

---
+++ P43-procesa-calificacion/Program.cs
@@ -0,0 +1,18 @@
+﻿// Procesa n calificaciones, calcula la suma y el promedio
+
+int c, n;
+float calif=0, suma=0, promedio=0;
+
+Console.Clear();
+Console.WriteLine("Procesa n calificaciones,calcula la suma y el promedio");
+Console.Write("Cuantas calificaciones ?");
+n = int.Parse(Console.ReadLine());
+c = 1;
+while (c <=n){
+    Console.Write($"Calificacion {c++} ?");
+    calif = float.Parse(Console.ReadLine());
+    suma += calif;
+
+}
+promedio = suma / n;
+Console.WriteLine($"La suma es {suma:f2} y el promedio es {promedio:f2}");
---
+++ P44-tabla-multiplicar/Program.cs
@@ -0,0 +1,16 @@
+﻿// Imprimir la tabla de multiplicar que el usuario la pida hasta donde la pida
+
+int c, t, n;
+Console.Clear();
+Console.WriteLine("Imprir la tabla de multiplicar que el usuario pida hasta donde la pida \n");
+Console.Write("Cual tabla quieres ?");  t = int.Parse(Console.ReadLine());
+Console.Write("Hasta donde");       n = int.Parse(Console.ReadLine());
+
+
+c=1;
+while (c<=n){
+    Console.WriteLine($"{t,3} x {c,3} = {t*c}");
+    c++;
+
+}
+
