03ca62c63d0f3a8d7990e02457971e75b0b01f41
38192392@uaz.edu.mx <38192392@uaz.edu.mx>
2023-03-14 11:23:00 -0600

    Actividad 9

---
+++ P46-pares-ascendentes/Program.cs
@@ -0,0 +1,23 @@
+﻿// Imprime ,los numeros pares de 2 a n, clcula su suma, el proceso se repite
+
+int n, c, s;
+char resp;
+
+do{
+Console.Clear();
+Console.WriteLine("Imprime ,los numeros pares de 2 a n, clcula su suma, el proceso se repite");
+Console.Write("Hasta donde deseas los pares ?"); n= int.Parse(Console.ReadLine());
+
+c=2;
+s=0;
+while( c<= n){
+    Console.Write($"{c} ");
+    s += c;
+    c +=2;
+    
+}
+Console.WriteLine($"\n La suma de los números pares de 2 al {n} es = {s}");
+Console.Write("\nDeseas continuar (S/N) ? ");resp = char.ToUpper(Console.ReadLine()[0]);
+} while (resp != 'N');
+
+Console.WriteLine("\n Proceso termiando, ya me canse de repetir ...");
---
+++ P47-impares-descendente/Program.cs
@@ -0,0 +1,23 @@
+﻿// Imprime ,los numeros impares  de n al 1, clcula su suma, el proceso se repite
+
+int n, c, s;
+char resp;
+
+do{
+Console.Clear();
+Console.WriteLine("Imprime ,los numeros impares  de n al 1, clcula su suma, el proceso se repite");
+Console.Write("Hasta donde deseas los impares ?"); n= int.Parse(Console.ReadLine());
+
+c=( n%2==0 ? --n: n);
+s=0;
+while( c>= 1){
+    Console.Write($"{c} ");
+    s += c;
+    c -=2;
+    
+}
+Console.WriteLine($"\n La suma de los números pares de {n}  al 1 es = {s}");
+Console.Write("\nDeseas continuar (S/N) ? ");resp = char.ToUpper(Console.ReadLine()[0]);
+} while (resp != 'N');
+
+Console.WriteLine("\n Proceso termiando, ya me canse de repetir ...");
